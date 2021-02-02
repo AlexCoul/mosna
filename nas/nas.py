cf646db7bf8ecc574d497bd27c9975c58ced1c75
AlexCoul <alexis.coullomb@gmail.com>
2021-02-02 10:18:11 +0100

    add main NAS functions

--- nas/nas.py
+++ nas/nas.py
@@ -348,3 +348,227 @@ def zscore(mat, mat_rand, axis=0, return_stats=False):
         return zscore
 
 ############ Neighbors Aggegation Statistics ############
+
+def neighbors(pairs, n):
+    """
+    Return the list of neighbors of a node in a network defined 
+    by edges between pairs of nodes. 
+    
+    Parameters
+    ----------
+    pairs : array_like
+        Pairs of nodes' id that define the network's edges.
+    n : int
+        The node for which we look for the neighbors.
+        
+    Returns
+    -------
+    neigh : array_like
+        The indices of neighboring nodes.
+    """
+    
+    left_neigh = pairs[pairs[:,1] == n, 0]
+    right_neigh = pairs[pairs[:,0] == n, 1]
+    neigh = np.hstack( (left_neigh, right_neigh) ).flatten()
+    
+    return neigh
+
+def neighbors_k_order(pairs, n, order):
+    """
+    Return the list of up the kth neighbors of a node 
+    in a network defined by edges between pairs of nodes
+    
+    Parameters
+    ----------
+    pairs : array_like
+        Pairs of nodes' id that define the network's edges.
+    n : int
+        The node for which we look for the neighbors.
+    order : int
+        Max order of neighbors.
+        
+    Returns
+    -------
+    all_neigh : list
+        The list of lists of 1D array neighbor and the corresponding order
+    
+    
+    Examples
+    --------
+    >>> pairs = np.array([[0, 10],
+                        [0, 20],
+                        [0, 30],
+                        [10, 110],
+                        [10, 210],
+                        [10, 310],
+                        [20, 120],
+                        [20, 220],
+                        [20, 320],
+                        [30, 130],
+                        [30, 230],
+                        [30, 330],
+                        [10, 20],
+                        [20, 30],
+                        [30, 10],
+                        [310, 120],
+                        [320, 130],
+                        [330, 110]])
+    >>> neighbors_k_order(pairs, 0, 2)
+    [[array([0]), 0],
+     [array([10, 20, 30]), 1],
+     [array([110, 120, 130, 210, 220, 230, 310, 320, 330]), 2]]
+    """
+    
+    # all_neigh stores all the unique neighbors and their oder
+    all_neigh = [[np.array([n]), 0]]
+    unique_neigh = np.array([n])
+    
+    for k in range(order):
+        # detected neighbor nodes at the previous order
+        last_neigh = all_neigh[k][0]
+        k_neigh = []
+        for node in last_neigh:
+            # aggregate arrays of neighbors for each previous order neighbor
+            neigh = np.unique(neighbors(pairs, node))
+            k_neigh.append(neigh)
+        # aggregate all unique kth order neighbors
+        if len(k_neigh) > 0:
+            k_unique_neigh = np.unique(np.concatenate(k_neigh, axis=0))
+            # select the kth order neighbors that have never been detected in previous orders
+            keep_neigh = np.in1d(k_unique_neigh, unique_neigh, invert=True)
+            k_unique_neigh = k_unique_neigh[keep_neigh]
+            # register the kth order unique neighbors along with their order
+            all_neigh.append([k_unique_neigh, k+1])
+            # update array of unique detected neighbors
+            unique_neigh = np.concatenate([unique_neigh, k_unique_neigh], axis=0)
+        else:
+            break
+        
+    return all_neigh
+
+def flatten_neighbors(all_neigh):
+    """
+    Convert the list of neighbors 1D arrays with their order into
+    a single 1D array of neighbors.
+
+    Parameters
+    ----------
+    all_neigh : list
+        The list of lists of 1D array neighbor and the corresponding order.
+
+    Returns
+    -------
+    flat_neigh : array_like
+        The indices of neighboring nodes.
+        
+    Examples
+    --------
+    >>> all_neigh = [[np.array([0]), 0],
+                     [np.array([10, 20, 30]), 1],
+                     [np.array([110, 120, 130, 210, 220, 230, 310, 320, 330]), 2]]
+    >>> flatten_neighbors(all_neigh)
+    array([  0,  10,  20,  30, 110, 120, 130, 210, 220, 230, 310, 320, 330])
+        
+    Notes
+    -----
+    For future features it should return a 2D array of
+    nodes and their respective order.
+    """
+    
+    list_neigh = []
+    for neigh, order in all_neigh:
+        list_neigh.append(neigh)
+    flat_neigh = np.concatenate(list_neigh, axis=0)
+
+    return flat_neigh
+
+def aggregate_k_neighbors(X, pairs, order=1, var_names=None):
+    """
+    Compute the statistics on aggregated variables across
+    the k order neighbors of each node in a network.
+
+    Parameters
+    ----------
+    X : array_like
+        The data on which to compute statistics (mean, std, ...).
+    pairs : array_like
+        Pairs of nodes' id that define the network's edges.
+    order : int
+        Max order of neighbors.
+    var_names : list
+        Names of variables of X.
+
+    Returns
+    -------
+    aggregg : dataframe
+        Statistics of X.
+        
+    Examples
+    --------
+    >>> pairs = vor.ridge_points[selection,:]
+    >>> genes_aggreg = aggregate_k_neighbors(X=Xpred, pairs=pairs, order=2)
+    """
+    
+    nb_obs = X.shape[0]
+    nb_var = X.shape[1]
+    aggreg = np.zeros((nb_obs, nb_var*2)) # *2 because mean and variance are stored
+
+    for i in range(nb_obs):
+        all_neigh = neighbors_k_order(pairs, n=i, order=order)
+        neigh = flatten_neighbors(all_neigh)
+        aggreg[i,:nb_var] = X[neigh,:].mean(axis=0)
+        aggreg[i,-nb_var:] = X[neigh,:].std(axis=0)
+    
+    if var_names is None:
+        var_names = [str(i) for i in range(nb_var)]
+    columns = [var + ' mean' for var in var_names] +\
+              [var + ' std' for var in var_names]
+    aggreg = pd.DataFrame(data=aggreg, columns=columns)
+    
+    return aggreg
+
+def make_cluster_cmap(labels, grey_pos='start'):
+    """
+    Creates an appropriate colormap for a vector of cluster labels.
+    
+    Parameters
+    ----------
+    labels : array_like
+        The labels of multiple clustered points
+    grey_pos: str
+        Where to put the grey color for the noise
+    
+    Returns
+    -------
+    cmap : matplotlib colormap object
+        A correct colormap
+    
+    Examples
+    --------
+    >>> my_cmap = make_cluster_cmap(labels=np.array([-1,3,5,2,4,1,3,-1,4,2,5]))
+    """
+    
+    from matplotlib.colors import ListedColormap
+    
+    if labels.max() < 9:
+        cmap = list(plt.get_cmap('tab10').colors)
+        if grey_pos == 'end':
+            cmap.append(cmap.pop(-3))
+        elif grey_pos == 'start':
+            cmap = [cmap.pop(-3)] + cmap
+        elif grey_pos == 'del':
+            del cmap[-3]
+    else:
+        cmap = list(plt.get_cmap('tab20').colors)
+        if grey_pos == 'end':
+            cmap.append(cmap.pop(-6))
+            cmap.append(cmap.pop(-6))
+        elif grey_pos == 'start':
+            cmap = [cmap.pop(-5)] + cmap
+            cmap = [cmap.pop(-5)] + cmap
+        elif grey_pos == 'del':
+            del cmap[-5]
+            del cmap[-5]
+    cmap = ListedColormap(cmap)
+    
+    return cmap
